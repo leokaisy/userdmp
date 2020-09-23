6b9b67e376407e91b589e17898e7f5e0a05976ba
kaisy <18170070825@163.com>
2020-09-23 15:09:13 +0800

    str 2 util

---
+++ src/main/scala/com/ng/bigdata/util/String2Util.scala
@@ -0,0 +1,23 @@
+package com.ng.bigdata.util
+
+/**
+ * @User: kaisy
+ * @Date: 2020/9/23 15:01
+ * @Desc:
+ */
+object String2Util {
+  def str2Int(str: String) = {
+    try{
+      str.toInt
+    }catch{
+      case _:Exception => 0
+    }
+  }
+  def str2Double(str: String) = {
+    try{
+      str.toDouble
+    }catch{
+      case _:Exception => 0.0
+    }
+  }
+}
